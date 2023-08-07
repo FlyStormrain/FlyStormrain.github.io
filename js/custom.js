5462f9e5fdbebb3987830ef7f735195df807ca89
FlyStormrain <shaoyuanwang2000@icloud.com>
2023-08-07 16:59:28 +0800

    Site updated: 2023-08-07 16:59:27

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀我好想你啊';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '太好了，你回来了～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
