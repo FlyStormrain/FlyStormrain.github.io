cd7d351e2f34a03e87b2eb96120bfd789ee64fc1
FlyStormrain <shaoyuanwang2000@icloud.com>
2023-08-08 18:54:10 +0800

    Site updated: 2023-08-08 18:54:10

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "CCCCCC",
+    "citySize": "16",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "15px 429px 10px 20px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "49b32ebd62934603a9a9441cc23c88f7"
+  }
+}
+
+
